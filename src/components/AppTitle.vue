6523f4e416c58e4c1d654529ea75c835da373fff
alberto <alberto.taddeucci@gmail.com>
2024-02-28 14:43:46 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppTitle'
+}
+
+</script>
+
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style>
+h1 {
+    padding: 2em;
+    text-align: center;
+    font-style: italic;
+
+}
+</style>
